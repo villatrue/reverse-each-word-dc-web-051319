881691c6e1aceef0ce4851cedd7286f840158aeb
Sean Carter <scarter0313@gmail.com>
2019-05-08 21:10:17 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,18 @@
+# def reverse_each_word(sentence)
+#   new_array = []
+#   sentence.split.each do |word|
+#     new_array.push(word.reverse)
+#   end
+#   return new_array.join(" ")
+# end
+
+# # def change_nums(nums)
+# #   nums.each do |x|
+# #     puts x + 1
+# #   end
+# # end
+ 
+# # change_nums(cool_nums)
 
 
 def reverse_each_word(sentence)
